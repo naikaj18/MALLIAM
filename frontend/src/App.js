970d6b2622aaaf95a39f23aa6b66a07b685f7ed9
Naikaj <naikaj18@gmail.com>
2025-03-20 00:56:48 -0700

    Fixed frontend tracking issue

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message))
+      .catch((err) => console.error("Error:", err));
+  }, []);
+
+  return (
+    <div>
+      <h1>Mailliam – AI Email Summarizer</h1>
+      <h2>{message}</h2>
+    </div>
+  );
+}
+
+export default App;
